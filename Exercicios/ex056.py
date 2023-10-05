11e6ff81f10730e025572792896178953b8ac991
Wallyson Silva <walliyy@live.com>
2023-10-05 07:49:00 -0300

    new exercises

--- Exercicios/ex056.py
+++ Exercicios/ex056.py
@@ -7,11 +7,28 @@ idade = []
 sexo = []
 
 for c in range(1,5):
-    nome.append(str(input('Digite seu nome: ')))
+    nome.append(str(input('Digite seu nome: ')).upper())
     idade.append(int(input('Digite sua idade: ')))
-    sexo.append(str(input('Escolha M - Masculino ou F - Feminino: ')))
-print(nome)
-print(idade)
-print(sexo)
+    sexo.append(str(input('Escolha M - Masculino ou F - Feminino: ')).upper())
 
-print(f'A media de idade do grupo é {(sum(idade))/4}')
+# for i, op in enumerate(sexo):
+#     if op == 'M':
+#         maior_idade = max(idade)
+#         id_maior_idade = idade.index(maior_idade)
+#         print(f'O homem mais velho é {nome[id_maior_idade]} que tem {maior_idade} anos')
+
+masculino = 'M'
+limite = 20
+cont = 0 
+
+if masculino in sexo:
+    maior_idade = max(idade)
+    id_maior_idade = idade.index(maior_idade)
+elif masculino not in sexo:    
+    for idades in idade:
+        if idades > limite:
+            cont += 1
+
+print(f'-> A media de idade do grupo é {(sum(idade))/4}')
+print(f'-> O homem mais velho é {nome[id_maior_idade]} que tem {maior_idade} anos')
+print(f'-> A quantidade de mulheres menores que 20 é {cont}')
