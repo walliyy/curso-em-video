6b4cc467e9d030c1952a21a2630288bddc6fcef1
Wallyson Silva <walliyy@live.com>
2023-10-16 07:40:19 -0300

    new exercises

--- Exercicios/ex056.py
+++ Exercicios/ex056.py
@@ -32,3 +32,29 @@ if len(nome) == len(idade) == len(sexo):
     print(f'-> A quantidade de mulheres menores que 20 é {cont}')
 else:
     print('Erro')
+
+# Solução do professor
+somaIdade = 0
+mediaIdade = 0
+maiorIdadeHomem = 0
+nomeVelho = ''
+totMulher20 = 0
+for p in range(1, 5):
+    print(f'----- {p}º PESSOA -----')
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaIdade += idade
+    if p == 1 and sexo in 'Mm':
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Mm' and idade > maiorIdadeHomem:
+        maiorIdadeHomem = idade
+        nomeVelho = nome
+    if sexo in 'Ff' and idade > 20:
+        totMulher20 += 1
+
+mediaIdade = somaIdade / 4
+print(f'A média de idade do grupo é de {mediaIdade} anos')
+print(f'O homem mais velho é o {nomeVelho}, com {maiorIdadeHomem} anos')
+print(f'Ao todo são {totMulher20} mulher(es) com menos de 20 anos')
