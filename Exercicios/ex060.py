c34b9802ce6b52c236863c389b1ca0bf176ab53c
Wallyson Silva <walliyy@live.com>
2023-10-26 07:41:58 -0300

    new exercises

--- Exercicios/ex060.py
+++ Exercicios/ex060.py
@@ -1,2 +1,19 @@
 # Faça um programa que leia um numero qualquer e mostre o seu fatorial.
 # Ex.: 5! 5x4x3x2x1 = 120
+n = fat = int(input('Informe o número: '))
+contador = 0
+tot = 0
+
+while fat != 0:
+    tot = n * (fat-1)
+    n -= 1
+    fat == n
+    print(n)
+print(tot)
+# for c in range(n, 0, -1):
+#     print(c, end='x')
+#     fat = n * c
+#     n = c
+#     print(n)
+#     print(fat)
+# print(fat)
